cda7a5e58638123aaa7a30bc3f99cdfd2af3b784
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-04-18 12:43:13 -0700

    add methods to play and stop video based on percentage

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,3 +1,44 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  addEndAttributeTime(2);
+  playVideoToPercentage(.25); // pass in video percentage
 });
+
+
+function playVideoToPercentage(percentage) {
+    addEndAttributeTime(5 * percentage)    // stop at 4 seconds
+    var video = document.getElementById("my-video");
+    var starttime = video.currentTime;  // start at 2 seconds
+    video.defaultPlaybackRate = 1;
+
+    //handler should be bound first
+    video.addEventListener("timeupdate", pauseAtTime);
+
+    video.load();
+
+    video.play(); 
+    try {
+        video.currentTime = starttime;
+    } catch (ex) {
+        //handle exceptions here
+    }
+}
+
+function addEndAttributeTime(value){
+  var video = document.getElementById("my-video");
+  $(video).attr('data-end-time', value);
+};
+
+function getEndTime(){
+	var video = document.getElementById("my-video");
+	return parseFloat($(video).attr('data-end-time'));	
+};
+
+function pauseAtTime(){
+  var video = document.getElementById("my-video");
+  var endtime = getEndTime();
+
+  if (video.currentTime >= endtime) {
+      video.pause();
+  }
+};
+
