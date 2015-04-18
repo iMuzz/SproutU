04f2e81d66d6bec2f62992a8438ec30c0353facb
Bryan Grace <bdg310@gmail.com>
2015-04-18 11:53:42 -0700

    tabs working

---
+++ public/scripts/main.js
@@ -0,0 +1,57 @@
+console.log("linked");
+
+function removeAllContent() {
+
+	$('.dash-content1').removeClass('active');
+	$('.dash-content2').removeClass('active');
+	$('.dash-content3').removeClass('active');
+	$('.dash-content4').removeClass('active');
+	$('.dash-content5').removeClass('active');
+
+	$('.tab1').removeClass('active');
+	$('.tab2').removeClass('active');
+	$('.tab3').removeClass('active');
+	$('.tab4').removeClass('active');
+	$('.tab5').removeClass('active');
+}
+
+
+$(document).ready(function(){
+
+	$('.tab1').click(function(){
+		removeAllContent();
+
+		$('.dash-content1').addClass('active');
+		$('.tab1').addClass('active');
+	});
+
+	$('.tab2').click(function(){
+		removeAllContent();
+
+		$('.dash-content2').addClass('active');
+		$('.tab2').addClass('active');
+	});
+
+	$('.tab3').click(function(){
+		removeAllContent();
+
+		$('.dash-content3').addClass('active');
+		$('.tab3').addClass('active');
+	});
+
+	$('.tab4').click(function(){
+		removeAllContent();
+
+		$('.dash-content4').addClass('active');
+		$('.tab4').addClass('active');
+	});
+
+	$('.tab5').click(function(){
+		removeAllContent();
+
+		$('.dash-content5').addClass('active');
+		$('.tab5').addClass('active');
+	});
+
+
+});
