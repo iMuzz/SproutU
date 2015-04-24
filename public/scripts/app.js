ae103012974aedf40deee76a1e6aa9a5de6c6776
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-04-23 18:00:54 -0700

    angular linked up and working properly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,14 @@
 (function(){
-	var app = angular.module("creditDash", []);}
+	var app = angular.module('creditDash', []);
 
+	app.controller('ScoreController', function(){
+		this.test = test;
+		console.log("This is just to see if it works")
+	});
+
+	var test = {
+		name: 'Faraaz',
+		price: 3.0,
+		description: "Hey this should be the description"
+	}
 })();
